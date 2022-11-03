a844b1d601973f55aea6c4b92b69aff1fd739b54
Mai Tue <maivantue29@gmail.com>
2022-11-03 09:43:44 +0700

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const expressValidator = require('express-validator')
+const routes = require('./src/routes/routes')
+const mongoose = require('mongoose')
+
+const app = express()
+const PORT = process.env.PORT || 8080
+const db = mongoose.connection;
+
+dotenv.config()
+
+//connect db
+mongoose.connect(process.env.DB_URL, {  
+    useNewUrlParser: true, 
+    useUnifiedTopology: true }).then(() => console.log('DB Connected!'));
+db.on('error', (err) => {
+    console.log('DB connection error:', err.message);
+})
+
+app.use(morgan("dev"))
+app.use(bodyParser.json())
+
+app.use('/', routes)
+
+app.listen(PORT, () => {console.log("Server started on http://localhost:"+PORT)})
+
+module.exports = app;
---
+++ src/Models/UserModels.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    email: {type: String, unique: true, required: true, trim: true},
+    username: {type: String, required: true, trim: true, minlength: 2},
+    password: {type: String, required: true, trim: true, minlength: 6},
+    password_confirm: {type: String, required: true, trim: true, minlength: 6},
+});
+
+module.exports = mongoose.model('users', userSchema)
